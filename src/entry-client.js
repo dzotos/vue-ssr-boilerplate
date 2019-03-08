4d8ed524cfbb96090d2ed2a66b3e0c07cacb94e8
Dimitris Zotos <dzwtos@gmail.com>
2019-03-09 00:59:50 +0200

    Create base, client, server entry points for Webpack

---
+++ src/entry-client.js
@@ -0,0 +1,7 @@
+import {createApp} from './app';
+
+// client-specific bootstrapping logic...
+const {app} = createApp();
+
+// this assumes App.vue template root element has `id="app"`
+app.$mount('#app');
