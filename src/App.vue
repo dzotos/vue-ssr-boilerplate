ac7718a6bef849a1bc277757c1691c4f58cb9d81
Dimitris Zotos <dzwtos@gmail.com>
2019-03-09 00:56:31 +0200

    Create Vue dummy components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <p>Hello!</p>
+    <ul>
+      <li>
+        <router-link to="/">Navigate to Home</router-link>
+      </li>
+      <li>
+        <router-link to="/item">Navigate to Item</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        text: 'Home Component'
+      };
+    }
+  };
+</script>
