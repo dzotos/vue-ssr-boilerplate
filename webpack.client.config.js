8557b95be56a5a7860b16e4be898193febcf8009
Dimitris Zotos <dzwtos@gmail.com>
2019-03-09 00:52:15 +0200

    Create webpack client & server config

---
+++ webpack.client.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/entry-client.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: ['vue-style-loader', 'css-loader', 'sass-loader']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
+};
